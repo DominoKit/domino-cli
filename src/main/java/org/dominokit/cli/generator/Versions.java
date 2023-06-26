a8020f05c55a449f5aee41b8b7753cedd100685d
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2023-06-26 16:27:31 +0300

    update domino mvp version

--- src/main/java/org/dominokit/cli/generator/Versions.java
+++ src/main/java/org/dominokit/cli/generator/Versions.java
@@ -3,7 +3,7 @@ package org.dominokit.cli.generator;
 public enum Versions {
     domino_ui_version("DOMINO_CLI_DOMINO_UI_VERSION","1.0.0-RC19"),
     domino_history_version("DOMINO_CLI_DOMINO_HISTORY_VERSION","1.0.0-RC5"),
-    domino_mvp_version("DOMINO_CLI_DOMINO_MVP_VERSION","1.0.0-RC11"),
+    domino_mvp_version("DOMINO_CLI_DOMINO_MVP_VERSION","1.0.0-RC12"),
     domino_rest_version("DOMINO_CLI_DOMINO_REST_VERSION","1.0.0-RC7"),
     domino_jackson_version("DOMINO_CLI_DOMINO_JACKSON_VERSION","1.0.0-RC4"),
     quarkus_version("DOMINO_CLI_QUARKUS_VERSION","2.16.7.Final"),
