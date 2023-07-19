47d2be23321b89788179c3651aa7e6ede810315e
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2023-07-19 15:30:08 +0300

    use install4j for installation media

--- src/main/java/org/dominokit/cli/generator/Versions.java
+++ src/main/java/org/dominokit/cli/generator/Versions.java
@@ -1,11 +1,11 @@
 package org.dominokit.cli.generator;
 
 public enum Versions {
-    domino_ui_version("DOMINO_CLI_DOMINO_UI_VERSION","1.0.0-RC19"),
-    domino_history_version("DOMINO_CLI_DOMINO_HISTORY_VERSION","1.0.0-RC5"),
-    domino_mvp_version("DOMINO_CLI_DOMINO_MVP_VERSION","1.0.0-RC12"),
-    domino_rest_version("DOMINO_CLI_DOMINO_REST_VERSION","1.0.0-RC7"),
-    domino_jackson_version("DOMINO_CLI_DOMINO_JACKSON_VERSION","1.0.0-RC4"),
+    domino_ui_version("DOMINO_CLI_DOMINO_UI_VERSION","1.0.0"),
+    domino_history_version("DOMINO_CLI_DOMINO_HISTORY_VERSION","1.0.0"),
+    domino_mvp_version("DOMINO_CLI_DOMINO_MVP_VERSION","1.0.0"),
+    domino_rest_version("DOMINO_CLI_DOMINO_REST_VERSION","1.0.0"),
+    domino_jackson_version("DOMINO_CLI_DOMINO_JACKSON_VERSION","1.0.0"),
     quarkus_version("DOMINO_CLI_QUARKUS_VERSION","2.16.7.Final"),
     vertx_version("DOMINO_CLI_VERTX_VERSION","3.9.0"),
     gwt_version("DOMINO_CLI_GWT_VERSION","2.10.0"),
