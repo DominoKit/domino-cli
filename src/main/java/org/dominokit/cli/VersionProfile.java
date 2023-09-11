e1fe2f90a9894a53b797e5fdae29724cb199ddfb
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2023-09-11 16:25:18 +0300

    prepare for release 2.0.0-RC1

--- src/main/java/org/dominokit/cli/VersionProfile.java
+++ src/main/java/org/dominokit/cli/VersionProfile.java
@@ -42,25 +42,25 @@ public class VersionProfile {
         switch (version){
         case "v1" :
             return new VersionProfile(version, version,
-                    ToolVersion.of("domino_ui_version", "1.0.1"),
+                    ToolVersion.of("domino_ui_version", "1.0.2"),
                     ToolVersion.of("domino_history_version", "1.0.0"),
                     ToolVersion.of("domino_mvp_version", "1.0.0"),
                     ToolVersion.of("domino_rest_version", "1.0.0"),
                     ToolVersion.of("domino_jackson_version", "1.0.0"),
                     ToolVersion.of("quarkus_version", "2.16.7.Final"),
-                    ToolVersion.of("vertx_version", "3.9.0"),
+                    ToolVersion.of("vertx_version", "3.9.4"),
                     ToolVersion.of("gwt_version", "2.10.0"),
                     ToolVersion.of("j2cl_maven_plugin_version", "0.21")
                     );
         case "v2" :
             return new VersionProfile(version, version,
-                    ToolVersion.of("domino_ui_version", "2.0.0-RC1"),
+                    ToolVersion.of("domino_ui_version", "2.0.0-RC2"),
                     ToolVersion.of("domino_history_version", "1.0.0"),
-                    ToolVersion.of("domino_mvp_version", "HEAD-SNAPSHOT"),
+                    ToolVersion.of("domino_mvp_version", "2.0.0-RC1"),
                     ToolVersion.of("domino_rest_version", "1.0.0"),
                     ToolVersion.of("domino_jackson_version", "1.0.0"),
                     ToolVersion.of("quarkus_version", "2.16.7.Final"),
-                    ToolVersion.of("vertx_version", "3.9.0"),
+                    ToolVersion.of("vertx_version", "3.9.4"),
                     ToolVersion.of("gwt_version", "2.10.0"),
                     ToolVersion.of("j2cl_maven_plugin_version", "0.21")
                     );
@@ -72,7 +72,7 @@ public class VersionProfile {
                         ToolVersion.of("domino_rest_version", "HEAD-SNAPSHOT"),
                         ToolVersion.of("domino_jackson_version", "HEAD-SNAPSHOT"),
                         ToolVersion.of("quarkus_version", "2.16.7.Final"),
-                        ToolVersion.of("vertx_version", "3.9.0"),
+                        ToolVersion.of("vertx_version", "3.9.4"),
                         ToolVersion.of("gwt_version", "2.10.0"),
                         ToolVersion.of("j2cl_maven_plugin_version", "0.21")
                 );
